400b83b6aef12d1785c518b03bda951898681095
rpurciel <rpurciel@gmail.com>
2023-11-13 14:43:34 -0800

    stupid error fixed

--- app.py
+++ app.py
@@ -4,5 +4,11 @@ app = Flask(__name__)
 @app.route('/')
 def home():
    return render_template('index.html')
-if __name__ == '__main__':
-   app.run()
+
+
+def main():
+    port = int(os.environ.get('PORT', 33507))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
+if __name__ == "__main__":
+    main()
