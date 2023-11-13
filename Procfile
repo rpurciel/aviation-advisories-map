025f2800af9660e4001fbee144e1d5c1730865a6
rpurciel <rpurciel@gmail.com>
2023-11-13 14:46:13 -0800

    stupid error fixed

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn
+web: gunicorn -b 0.0.0.0:$PORT
 app: app
--- app.py
+++ app.py
@@ -5,10 +5,8 @@ app = Flask(__name__)
 def home():
    return render_template('index.html')
 
-
 def main():
-    port = int(os.environ.get('PORT', 33507))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0')
 
 if __name__ == "__main__":
     main()
